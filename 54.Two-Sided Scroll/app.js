7141a206ed7f2789eba5adf83ea79f8902ab3ee0
Buzzdler <140882843+Buzzdler@users.noreply.github.com>
2024-11-15 19:18:57 +0200

    Add files via upload

---
+++ 54.Two-Sided Scroll/app.js
@@ -0,0 +1,35 @@
+const sliderContainer = document.querySelector(".slider-container");
+const slideRight = document.querySelector(".right-slide");
+const slideLeft = document.querySelector(".left-slide");
+const upButton = document.getElementById("up-btn");
+const downButton = document.getElementById("down-btn");
+const sliderLength = slideRight.querySelectorAll("div").length;
+
+let activeSlideIndex = 0;
+slideLeft.style.top = `-${(sliderLength - 1) * 100}vh`;
+
+downButton.addEventListener("click", () => nextSlide("up"));
+upButton.addEventListener("click", () => nextSlide("down"));
+
+function nextSlide(params) {
+  const slideHeight = sliderContainer.clientHeight;
+
+  if (params === "up") {
+    activeSlideIndex++;
+    if (activeSlideIndex > sliderLength - 1) {
+      activeSlideIndex = 0;
+    }
+  }
+
+  if (params === "down") {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = sliderLength - 1;
+    }
+  }
+
+  slideRight.style.transform = `translateY(-${
+    activeSlideIndex * slideHeight
+  }px)`;
+  slideLeft.style.transform = `translateY(${activeSlideIndex * slideHeight}px)`;
+}
